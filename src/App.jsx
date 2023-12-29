bd622332ff39a55bddb984f3e5750510495f8635
Partha Paul <pparthapaul11@gmail.com>
2023-12-29 14:31:39 +0530

    base url

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 // src/App.jsx
 import React from 'react';
-import './input.css'
+import './input.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/Landing';
 import RegistrationForm from './components/RegistrationForm';
 import DashboardPage from './components/Dashboard';
 import Navbar from './components/Navbar';
+// import.
+console.log(import.meta.env.BASE_URL);
 
 const App = () => {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename={import.meta.env.BASE_URL} >
       <Routes>
         <Route path="/" exact element={<><Navbar/><LandingPage/></>}/>
         <Route path='/dashboard' element={<><Navbar/><DashboardPage/></>}/>
